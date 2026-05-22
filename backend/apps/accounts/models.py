7fad389d1490798503607fb9396c0671e7fdf673
sidddhesh-dev <siddhesh.s.thorat14@gmail.com>
2026-05-22 13:31:25 +0530

    UserProfile model or Database table created

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    fullname=models.CharField(max_length=100)
+    email=models.EmailField(unique=True)
+    create_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.fullname
