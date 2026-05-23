abc231d439dec30fa5602783b1392e9a199946de
sidddhesh-dev <siddhesh.s.thorat14@gmail.com>
2026-05-23 15:33:09 +0530

    First API created Tested API  first API view created API methods tried Serialization learnd

---
+++ backend/apps/accounts/serializers.py
@@ -0,0 +1,8 @@
+from apps.accounts.models import UserProfile
+from rest_framework import serializers
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=UserProfile
+        fields='__all__'
+
--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,11 +1,31 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import UserProfile
+from apps.accounts.serializers import UserProfileSerializer
 
-@api_view(['GET'])
-def homepage(request):
-    return Response(
-        {
-            "message":"account page lodingg successfully",
-            "status":"success"
-        }
-    )
+@api_view(['GET','POST'])
+def User_list(request):
+
+    if request.method=="GET":
+        user=UserProfile.objects.all()
+        serialize=UserProfileSerializer(user,many=True)
+        data=serialize.data
+        return Response(data)
+    
+    if request.method=="POST":
+        user=UserProfileSerializer(data=request.data)
+
+        if user.is_valid():
+            user.save()
+            return Response(
+                user.data,
+                status=status.HTTP_201_CREATED
+            )
+            
+        return Response(
+            user.errors,
+            status=status.HTTP_401_UNAUTHORIZED,
+        
+
+        )
