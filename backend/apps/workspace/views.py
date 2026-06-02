2f34f1f06bacb12e7138f5ba3d16c4348764fc39
sidddhesh-dev <siddhesh.s.thorat14@gmail.com>
2026-06-02 20:42:39 +0530

    Document search pipleline v2

--- backend/apps/workspace/views.py
+++ backend/apps/workspace/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.response import Response
 from .models import Note,Document
 from rest_framework import status
