0ba629d34e56bb4c4f7694a0287d9690354e315a
sidddhesh-dev <siddhesh.s.thorat14@gmail.com>
2026-06-18 21:01:19 +0530

    feat : Chromadb client service added with embedding service

--- backend/apps/workspace/services/chromadb_service.py
+++ backend/apps/workspace/services/chromadb_service.py
@@ -1,9 +1,42 @@
 import chromadb
 
 class VectorStoreService:
-
+    @staticmethod
     def get_client():
         client=chromadb.PersistentClient(
             path='./chromadb'
         )
         return client
+
+    @staticmethod
+    def get_collection():
+        client=VectorStoreService.get_client()
+        collection=client.get_or_create_collection(
+            name='document_chunks'
+        )
+        return collection
+    
+    @staticmethod
+    def add_chunks(document,chunks,embeddings):
+        ids = [
+        f"doc_{document.id}_chunk_{i}"
+        for i in range(len(chunks))
+    ]
+
+        metadatas = [
+        {
+            "document_id": document.id,
+            "chunk_index": i
+        }
+        for i in range(len(chunks))
+    ]
+
+        collection = VectorStoreService.get_collection()
+
+        collection.add(
+        documents=chunks,
+        embeddings=embeddings,
+        metadatas=metadatas,
+        ids=ids
+    )
+        
