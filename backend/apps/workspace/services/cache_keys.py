7d214a02fe4db23b05d50142deb2acc22cb12d76
sidddhesh-dev <siddhesh.s.thorat14@gmail.com>
2026-06-29 13:20:46 +0530

    feat : Embedding service cache done using redis

--- backend/apps/workspace/services/cache_keys.py
+++ backend/apps/workspace/services/cache_keys.py
@@ -1,9 +1,13 @@
 import hashlib
 
 
-def generate_cache_key(question: str) -> str:
+def generate_cache_key(question) :
     question_hash = hashlib.sha256(
         question.strip().lower().encode()
     ).hexdigest()
 
     return f"llm:{question_hash}"
+
+def generate_embedding_cache_key(text):
+    embedding_hash=hashlib.sha256(text.strip().lower().encode()).hexdigest()
+    return f"embedding:{embedding_hash}"
--- backend/apps/workspace/services/embedding_service.py
+++ backend/apps/workspace/services/embedding_service.py
@@ -1,11 +1,10 @@
 
 from sentence_transformers import SentenceTransformer
-
+from apps.workspace.services.cache_keys import (generate_embedding_cache_key)
+from apps.workspace.services.redis_service import RedisService
 
 class EmbeddingService:
-
     model = None
-
     @staticmethod
     def get_model():
 
@@ -21,12 +20,21 @@ class EmbeddingService:
 
     @staticmethod
     def generate_embedding(text):
+        cache_key = generate_embedding_cache_key(text)
+        cached_embedding = RedisService.get(cache_key)
+        if cached_embedding:
+            print("EMBEDDING CACHE HIT")
+            return cached_embedding
+
+        print("EMBEDDING CACHE MISS")
 
         model = EmbeddingService.get_model()
 
-        embedding = model.encode(text)
+        embedding = model.encode(text).tolist()
 
-        return embedding.tolist()
+        RedisService.set(key=cache_key,value=embedding,timeout=86400)
+
+        return embedding
     
     @staticmethod
     def generate_embeddings(texts):
